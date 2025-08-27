ab82f7265d093b896dc4efd2f18ed8d994a8ac09
Manujapabodhana <rpmanuja123@gmail.com>
2025-08-27 11:10:48 +0530

    gb

--- finance-tracker-frontend/src/index.js
+++ finance-tracker-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // optional styling
+import "./style.css"; // optional styling
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
